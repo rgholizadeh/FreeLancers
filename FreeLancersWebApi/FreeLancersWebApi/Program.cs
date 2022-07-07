167eabe51a0065cedef1f33f1b433156d77417f2
Reza Gholizadeh <r.gholizadehmail.com>
2022-07-07 10:28:36 +0100

    Delete Wetherservice

--- FreeLancersWebApi/FreeLancersWebApi/Program.cs
+++ FreeLancersWebApi/FreeLancersWebApi/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddSingleton<FreeLancerService>();
 builder.Services.AddControllers()
     .AddJsonOptions(
         options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
